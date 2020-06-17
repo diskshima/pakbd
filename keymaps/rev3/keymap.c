1a32c7ba66ab734cd49d49ea41bf37bbb114b4e4
Daisuke Shimamoto <diskshima@gmail.com>
2020-06-17 12:50:21 +0900

    Move mouse keys to the right

--- keymaps/rev3/keymap.c
+++ keymaps/rev3/keymap.c
@@ -21,12 +21,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,             KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT ,
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,             KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
              KC_LALT, KC_LCMD,    KC_SPC,                               KC_SPC,   TG(_RAISE),       KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT
-      ),
+  ),
   [_RAISE] = LAYOUT(
-    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5   , _______,   KC_F6  , KC_F7  , KC_F8  , KC_9   , KC_F10 , KC_F11  , KC_F12 , _______, _______,
-    _______, _______, WH_LEFT, WH_UP  , WH_DOWN, WH_RIGHT,            _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, MS_LEFT, MS_UP  , MS_DOWN, MS_RIGHT,            KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,  _______,
-    _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5   , _______,   KC_F6  , KC_F7  , KC_F8  , KC_9    , KC_F10 , KC_F11 , KC_F12 , _______, _______,
+    _______, _______, _______, _______, _______, _______ ,            WH_LEFT, WH_DOWN, WH_UP  , WH_RIGHT, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______ ,            MS_LEFT, MS_DOWN, MS_UP  , MS_RIGHT, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______ , _______, _______, _______,
              _______, _______,    KC_MS_BTN1,                           KC_MS_BTN2, _______,      _______, _______, _______, _______
   )
 };

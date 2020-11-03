c0ae52d3806dfaa408228155f8ca04bc30f9fd28
Daisuke Shimamoto <diskshima@gmail.com>
2020-11-03 15:39:35 +0900

    Enable function keys when holding key down

--- keymaps/rev3/config.h
+++ keymaps/rev3/config.h
@@ -29,6 +29,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 //#define MASTER_RIGHT
 // #define EE_HANDS
+//
+#define TAPPING_TOGGLE 2
+
 
 // Underglow
 /*
